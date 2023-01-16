cdf262999c9e729082bcc38702ac786d3870356f
WheatMillington <78046968+WheatMillington@users.noreply.github.com>
2023-01-17 09:27:34 +1300

    Update Form1.cs

--- FFGUI/Form1.cs
+++ FFGUI/Form1.cs
@@ -19,11 +19,6 @@ namespace FFGUI
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void btnExeLocation_Click(object sender, EventArgs e)
         {
             if ( openFileDialogFFMPEG.ShowDialog() == DialogResult.OK )
@@ -39,12 +34,14 @@ namespace FFGUI
                 txtInputFile.Text = openFileDialogInput.FileName;
 
                 string videoFilePath = openFileDialogInput.FileName;
-                string ffmpegOutput = RunFFmpegCommand("-i " + videoFilePath, txtFfmpegLocation.Text);
+                string ffmpegOutput = RunFFmpeg("-i " + videoFilePath, txtFfmpegLocation.Text);
                 int startIndex = ffmpegOutput.IndexOf("Duration: ") + "Duration: ".Length;
                 int endIndex = ffmpegOutput.IndexOf(",", startIndex);
                 string duration = ffmpegOutput.Substring(startIndex, endIndex - startIndex).Trim();
                 TimeSpan videoLength = TimeSpan.Parse(duration);
 
+                Console.WriteLine("Length in seconds: " + videoLength.TotalSeconds);
+                txtStart.Text = "0";
                 txtFinish.Text = videoLength.TotalSeconds.ToString();
             }
         }
@@ -68,57 +65,30 @@ namespace FFGUI
             }
             else
             {
+                // Generate a random number between 10 and 90 and append this to the file name before the extension
                 Random rnd = new Random();
-                outputFile = @"C:\ffmpeg\bin\vidout" + rnd.Next(10, 90) + ".mp4";
+                string dir = Path.GetDirectoryName(inputFile);
+                string file = Path.GetFileNameWithoutExtension(inputFile) + rnd.Next(10, 90);
+                string ext = Path.GetExtension(inputFile);
+                
+                outputFile = Path.Combine(dir, file, ext);
             }
-            Console.WriteLine("Output file: " + outputFile);
+
+            Console.WriteLine($"Output file: {outputFile}");
 
             // FFmpeg command to convert the video file
-            string command = $"-i {inputFile} -crf 18 -preset slow -c:a copy {outputFile}";
+            string command = $"-i {inputFile} -crf 18 -preset fast -c:a copy {outputFile}";
             Console.WriteLine("Using command: " + command);
 
             // Create a new process to run FFmpeg
-            var ffmpeg = new Process
-            {
-                StartInfo = new ProcessStartInfo
-                {
-                    FileName = txtFfmpegLocation.Text,
-                    Arguments = command,
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true,
-                    UseShellExecute = false,
-                    CreateNoWindow = true,
-                }
-            };
-            Console.WriteLine("Creating FFMPEG process using " + txtFfmpegLocation.Text);
-
-            // Start the process
-            ffmpeg.Start();
-            Console.WriteLine("Starting FFMPEG process");
-
-            // Read the output from FFmpeg
-            // I don't know why, but reading standard output causes this app to hang???
-            //Console.WriteLine("About to read Standard Output");
-            //var output = ffmpeg.StandardOutput.ReadToEnd();
+            string output = RunFFmpeg(command, txtFfmpegLocation.Text);
             
-            // On the other ahnd, reading error output is essential otherwise ffmpeg never closes
-            Console.WriteLine("About to read Error Output");
-            var error = ffmpeg.StandardError.ReadToEnd();
-
-
-            // Wait for the process to exit
-            Console.WriteLine("Waiting for process to exit");
-            ffmpeg.WaitForExit();
-            Console.WriteLine("Process has exited");
-
-            // Print the output
-            //Console.WriteLine(output);
-            Console.WriteLine(error);
+            Console.WriteLine("Creating FFMPEG process using " + txtFfmpegLocation.Text);
 
             lblStatus.Text = "Output file created at " + outputFile;
         }
 
-        static string RunFFmpegCommand(string arguments, string ffmpegloc)
+        static string RunFFmpeg(string arguments, string ffmpegloc)
         {
             Process process = new Process();
             process.StartInfo.FileName = ffmpegloc;
@@ -126,8 +96,15 @@ namespace FFGUI
             process.StartInfo.UseShellExecute = false;
             process.StartInfo.RedirectStandardOutput = true;
             process.Start();
-            string output = process.StandardOutput.ReadToEnd();
+            
+            var output = process.StandardOutput.ReadToEnd();
+            var error = process.StandardError.ReadToEnd();
+            
+            Console.WriteLine(output);
+            Console.WriteLine(error);
+            
             process.WaitForExit();
+            
             return output;
         }
     }
