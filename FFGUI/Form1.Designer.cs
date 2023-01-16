bca30693b510dda20447b581fbd964043fb8d7bf
Chris <contact@chrisdoms.com>
2023-01-16 21:54:30 +1300

    Add project files.

---
+++ FFGUI/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace FFGUI
+{
+    partial class frmGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFfmpegLocation = new System.Windows.Forms.TextBox();
+            this.txtInputFile = new System.Windows.Forms.TextBox();
+            this.lblExeLocation = new System.Windows.Forms.Label();
+            this.lblInputFile = new System.Windows.Forms.Label();
+            this.lblStart = new System.Windows.Forms.Label();
+            this.lblFinish = new System.Windows.Forms.Label();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.txtFinish = new System.Windows.Forms.TextBox();
+            this.chkOverwrite = new System.Windows.Forms.CheckBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.btnExeLocation = new System.Windows.Forms.Button();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.grpFileInfo = new System.Windows.Forms.GroupBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.openFileDialogFFMPEG = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialogInput = new System.Windows.Forms.OpenFileDialog();
+            this.grpFileInfo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtFfmpegLocation
+            // 
+            this.txtFfmpegLocation.Location = new System.Drawing.Point(133, 9);
+            this.txtFfmpegLocation.Name = "txtFfmpegLocation";
+            this.txtFfmpegLocation.Size = new System.Drawing.Size(399, 20);
+            this.txtFfmpegLocation.TabIndex = 0;
+            this.txtFfmpegLocation.Text = "C:\\ffmpeg\\bin\\ffmpeg.exe";
+            // 
+            // txtInputFile
+            // 
+            this.txtInputFile.Location = new System.Drawing.Point(133, 35);
+            this.txtInputFile.Name = "txtInputFile";
+            this.txtInputFile.Size = new System.Drawing.Size(399, 20);
+            this.txtInputFile.TabIndex = 1;
+            this.txtInputFile.Text = "C:\\ffmpeg\\bin\\vid.mp4";
+            // 
+            // lblExeLocation
+            // 
+            this.lblExeLocation.AutoSize = true;
+            this.lblExeLocation.Location = new System.Drawing.Point(13, 12);
+            this.lblExeLocation.Name = "lblExeLocation";
+            this.lblExeLocation.Size = new System.Drawing.Size(110, 13);
+            this.lblExeLocation.TabIndex = 2;
+            this.lblExeLocation.Text = "FFMPEG exe location";
+            this.lblExeLocation.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblInputFile
+            // 
+            this.lblInputFile.AutoSize = true;
+            this.lblInputFile.Location = new System.Drawing.Point(12, 38);
+            this.lblInputFile.Name = "lblInputFile";
+            this.lblInputFile.Size = new System.Drawing.Size(87, 13);
+            this.lblInputFile.TabIndex = 3;
+            this.lblInputFile.Text = "Input file location";
+            // 
+            // lblStart
+            // 
+            this.lblStart.AutoSize = true;
+            this.lblStart.Location = new System.Drawing.Point(12, 65);
+            this.lblStart.Name = "lblStart";
+            this.lblStart.Size = new System.Drawing.Size(78, 13);
+            this.lblStart.TabIndex = 4;
+            this.lblStart.Text = "Start (seconds)";
+            // 
+            // lblFinish
+            // 
+            this.lblFinish.AutoSize = true;
+            this.lblFinish.Location = new System.Drawing.Point(12, 89);
+            this.lblFinish.Name = "lblFinish";
+            this.lblFinish.Size = new System.Drawing.Size(83, 13);
+            this.lblFinish.TabIndex = 5;
+            this.lblFinish.Text = "Finish (seconds)";
+            // 
+            // txtStart
+            // 
+            this.txtStart.Location = new System.Drawing.Point(133, 62);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(100, 20);
+            this.txtStart.TabIndex = 6;
+            // 
+            // txtFinish
+            // 
+            this.txtFinish.Location = new System.Drawing.Point(133, 89);
+            this.txtFinish.Name = "txtFinish";
+            this.txtFinish.Size = new System.Drawing.Size(100, 20);
+            this.txtFinish.TabIndex = 7;
+            // 
+            // chkOverwrite
+            // 
+            this.chkOverwrite.AutoSize = true;
+            this.chkOverwrite.Location = new System.Drawing.Point(15, 119);
+            this.chkOverwrite.Name = "chkOverwrite";
+            this.chkOverwrite.Size = new System.Drawing.Size(128, 17);
+            this.chkOverwrite.TabIndex = 8;
+            this.chkOverwrite.Text = "Overwrite source file?";
+            this.chkOverwrite.UseVisualStyleBackColor = true;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(15, 143);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 9;
+            this.btnGo.Text = "Go!";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // btnExeLocation
+            // 
+            this.btnExeLocation.Location = new System.Drawing.Point(539, 7);
+            this.btnExeLocation.Name = "btnExeLocation";
+            this.btnExeLocation.Size = new System.Drawing.Size(106, 23);
+            this.btnExeLocation.TabIndex = 10;
+            this.btnExeLocation.Text = "Find FFMPEG";
+            this.btnExeLocation.UseVisualStyleBackColor = true;
+            this.btnExeLocation.Click += new System.EventHandler(this.btnExeLocation_Click);
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(539, 35);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(106, 23);
+            this.btnOpenFile.TabIndex = 11;
+            this.btnOpenFile.Text = "Open";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // grpFileInfo
+            // 
+            this.grpFileInfo.Controls.Add(this.lblStatus);
+            this.grpFileInfo.Location = new System.Drawing.Point(251, 65);
+            this.grpFileInfo.Name = "grpFileInfo";
+            this.grpFileInfo.Size = new System.Drawing.Size(393, 100);
+            this.grpFileInfo.TabIndex = 12;
+            this.grpFileInfo.TabStop = false;
+            this.grpFileInfo.Text = "File Info";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(6, 16);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 0;
+            // 
+            // openFileDialogFFMPEG
+            // 
+            this.openFileDialogFFMPEG.FileName = "openFileDialogFFMPEG";
+            this.openFileDialogFFMPEG.Title = "Locate the FFMPEG Executable";
+            // 
+            // openFileDialogInput
+            // 
+            this.openFileDialogInput.FileName = "openFileDialogInput";
+            this.openFileDialogInput.Title = "Select an input file";
+            // 
+            // frmGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 178);
+            this.Controls.Add(this.grpFileInfo);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.btnExeLocation);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.chkOverwrite);
+            this.Controls.Add(this.txtFinish);
+            this.Controls.Add(this.txtStart);
+            this.Controls.Add(this.lblFinish);
+            this.Controls.Add(this.lblStart);
+            this.Controls.Add(this.lblInputFile);
+            this.Controls.Add(this.lblExeLocation);
+            this.Controls.Add(this.txtInputFile);
+            this.Controls.Add(this.txtFfmpegLocation);
+            this.Name = "frmGUI";
+            this.Text = "FFMPEG GUI";
+            this.grpFileInfo.ResumeLayout(false);
+            this.grpFileInfo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFfmpegLocation;
+        private System.Windows.Forms.TextBox txtInputFile;
+        private System.Windows.Forms.Label lblExeLocation;
+        private System.Windows.Forms.Label lblInputFile;
+        private System.Windows.Forms.Label lblStart;
+        private System.Windows.Forms.Label lblFinish;
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.TextBox txtFinish;
+        private System.Windows.Forms.CheckBox chkOverwrite;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Button btnExeLocation;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.GroupBox grpFileInfo;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.OpenFileDialog openFileDialogFFMPEG;
+        private System.Windows.Forms.OpenFileDialog openFileDialogInput;
+    }
+}
+
---
+++ FFGUI/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FFGUI
+{
+    public partial class frmGUI : Form
+    {
+        public frmGUI()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExeLocation_Click(object sender, EventArgs e)
+        {
+            if ( openFileDialogFFMPEG.ShowDialog() == DialogResult.OK )
+            {
+                txtFfmpegLocation.Text = openFileDialogFFMPEG.FileName;
+            }
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            if ( openFileDialogInput.ShowDialog() == DialogResult.OK )
+            {
+                txtInputFile.Text = openFileDialogInput.FileName;
+
+                string videoFilePath = openFileDialogInput.FileName;
+                string ffmpegOutput = RunFFmpegCommand("-i " + videoFilePath, txtFfmpegLocation.Text);
+                int startIndex = ffmpegOutput.IndexOf("Duration: ") + "Duration: ".Length;
+                int endIndex = ffmpegOutput.IndexOf(",", startIndex);
+                string duration = ffmpegOutput.Substring(startIndex, endIndex - startIndex).Trim();
+                TimeSpan videoLength = TimeSpan.Parse(duration);
+
+                txtFinish.Text = videoLength.TotalSeconds.ToString();
+            }
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            string outputFile;
+            
+            lblStatus.Text = "Processing....";
+            lblStatus.Refresh();
+
+            // Input file path
+            string inputFile = txtInputFile.Text;
+            Console.WriteLine("Input file: " + inputFile);
+
+            // Output file path
+            if (chkOverwrite.Checked)
+            {
+                //currently seems to hang???
+                outputFile = inputFile;
+            }
+            else
+            {
+                Random rnd = new Random();
+                outputFile = @"C:\ffmpeg\bin\vidout" + rnd.Next(10, 90) + ".mp4";
+            }
+            Console.WriteLine("Output file: " + outputFile);
+
+            // FFmpeg command to convert the video file
+            string command = $"-i {inputFile} -crf 18 -preset slow -c:a copy {outputFile}";
+            Console.WriteLine("Using command: " + command);
+
+            // Create a new process to run FFmpeg
+            var ffmpeg = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = txtFfmpegLocation.Text,
+                    Arguments = command,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+            Console.WriteLine("Creating FFMPEG process using " + txtFfmpegLocation.Text);
+
+            // Start the process
+            ffmpeg.Start();
+            Console.WriteLine("Starting FFMPEG process");
+
+            // Read the output from FFmpeg
+            // I don't know why, but reading standard output causes this app to hang???
+            //Console.WriteLine("About to read Standard Output");
+            //var output = ffmpeg.StandardOutput.ReadToEnd();
+            
+            // On the other ahnd, reading error output is essential otherwise ffmpeg never closes
+            Console.WriteLine("About to read Error Output");
+            var error = ffmpeg.StandardError.ReadToEnd();
+
+
+            // Wait for the process to exit
+            Console.WriteLine("Waiting for process to exit");
+            ffmpeg.WaitForExit();
+            Console.WriteLine("Process has exited");
+
+            // Print the output
+            //Console.WriteLine(output);
+            Console.WriteLine(error);
+
+            lblStatus.Text = "Output file created at " + outputFile;
+        }
+
+        static string RunFFmpegCommand(string arguments, string ffmpegloc)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = ffmpegloc;
+            process.StartInfo.Arguments = arguments;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            return output;
+        }
+    }
+}
